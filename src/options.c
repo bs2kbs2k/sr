77c3e55e6addccb117e73cf79c8e9a4b3432e77a
Doggo <fearlessdoggo21@vivaldi.net>
2022-07-18 13:22:23 -0500

    master: rewrite options.*

--- src/options.c
+++ src/options.c
@@ -1,299 +1,119 @@
-/* options.c
+/* sr - screenshot utility
+ * Copyright (C) 2022 ArcNyxx
+ * see LICENCE file for licensing information */
 
-Copyright 1999-2000 Tom Gilbert <tom@linuxbrit.co.uk,
-                                  gilbertt@linuxbrit.co.uk,
-                                  scrot_sucks@linuxbrit.co.uk>
-Copyright 2008      William Vera <billy@billy.com.mx>
-Copyright 2009      George Danchev <danchev@spnet.net>
-Copyright 2009      James Cameron <quozl@us.netrek.org>
-Copyright 2010      Ibragimov Rinat <ibragimovrinat@mail.ru>
-Copyright 2017      Stoney Sauce <stoneysauce@gmail.com>
-Copyright 2019      Daniel Lublin <daniel@lublin.se>
-Copyright 2019-2022 Daniel T. Borelli <danieltborelli@gmail.com>
-Copyright 2019      Jade Auer <jade@trashwitch.dev>
-Copyright 2020      Sean Brennan <zettix1@gmail.com>
-Copyright 2021      Christopher R. Nelson <christopher.nelson@languidnights.com>
-Copyright 2021-2022 Guilherme Janczak <guilherme.janczak@yandex.com>
-Copyright 2021      IFo Hancroft <contact@ifohancroft.com>
-Copyright 2021      Peter Wu <peterwu@hotmail.com>
-Copyright 2021      Wilson Smith <01wsmith+gh@gmail.com>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to
-deal in the Software without restriction, including without limitation the
-rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-sell copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies of the Software and its documentation and acknowledgment shall be
-given in the documentation and software packages that this Software was
-used.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-*/
-
-#include <assert.h>
-#include <err.h>
+#include <ctype.h>
 #include <errno.h>
-#include <getopt.h>
 #include <limits.h>
-#include <stdbool.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
-#include "config.h"
-#include "options.h"
-#include "scrot.h"
-#include "scrot_selection.h"
+#include "opt.h"
+#include "util.h"
 
-#define STR_LEN_MAX_FILENAME(msg, fileName) do {                \
-    if (strlen((fileName)) > MAX_FILENAME) {                    \
-        errx(EXIT_FAILURE, #msg " filename too long, must be "  \
-            "less than %d characters", MAX_FILENAME);           \
-    }                                                           \
-} while(0)
+static int parse_int(const char *num);
+static void parse_asel(options_t *opt, char *const fmt);
+static void parse_line(options_t *opt, char *const fmt);
 
-#define checkMaxOutputFileName(fileName) \
-    STR_LEN_MAX_FILENAME(output, (fileName))
-
-#define checkMaxInputFileName(fileName) \
-    STR_LEN_MAX_FILENAME(input, (fileName))
-
-enum {
-    MAX_LEN_WINDOW_CLASS_NAME = 80, //characters
-    MAX_FILENAME = 256, // characters
-    MAX_DISPLAY_NAME = 256, // characters
-};
-
-struct ScrotOptions opt = {
-    .lineStyle = LineSolid,
-    .lineWidth = 1,
-    .lineOpacity = SELECTION_OPACITY_DEFAULT,
-    .lineMode = LINE_MODE_S_CLASSIC,
-};
-
-char *estrdup(const char *str)
+static int
+parse_int(const char *num)
 {
-    char *p;
-    if ((p = strdup(str)) == NULL)
-        err(EXIT_FAILURE, "strdup");
-    return p;
+	int ret;
+	if (!isdigit(num[0]) || (ret = strtol(num, NULL, 10)) < INT_MIN ||
+			ret > INT_MAX)
+		errno = 1;
+	return ret;
 }
 
-int optionsParseRequiredNumber(const char *str)
+static void
+parse_asel(options_t *opt, char *const fmt)
 {
-    assert(NULL != str); // fix yout caller function,
-                         //  the user does not impose this behavior
-    char *end = NULL;
-    long ret = 0L;
-    errno = 0;
-
-    ret = strtol(str, &end, 10);
-
-    if (errno)
-        goto range_error;
-
-    if (str == end)
-        errx(EXIT_FAILURE, "the option is not a number: %s", end);
-
-    if (ret > INT_MAX || ret < INT_MIN) {
-        errno = ERANGE;
-        goto range_error;
-    }
-
-    return ret;
-
-range_error:
-    err(EXIT_FAILURE, "error strtol");
+	int i;
+	char *start = fmt, *end;
+	for (i = 0; (end = strchr(start, ',')) != NULL; ++i) {
+		*end = '\0', opt.arr[i] = parse_int(start);
+		if (errno != 0)
+			die("sr: invalid option to -a: %s\n", start);
+		start = end + 1;
+	}
+	if (i != 4)
+		die("sr: invalid arument to -a: %s\n", fmt);
 }
 
-int optionsParseRequireRange(int n, int lo, int hi)
+static void
+parse_line(options_t *opt, char *const fmt)
 {
-    return (n < lo ? lo : n > hi ? hi : n);
+	char *list = fmt, *value;
+	while (list[0] != '\0') {
+		int index = getsubopt(&list, (char *[]){ "style", "width",
+				"color", "opacity", "mode", NULL }, &value);
+		if (value == NULL || value[0] == '\0' || index == -1)
+			die("sr: invalid argument to -l: %s\n", fmt);
+		switch (value) {
+		case 0:
+			if (!strcmp(value, "dash"))
+				opt.lstyle = LineDash;
+			else if (!strcmp(value, "solid"))
+				opt.lstyle = LineSolid;
+			else
+				die("sr: invalid option to -l: %s\n", value);
+			break;
+		case 1:
+			opt.lwidth = parse_int(value);
+			if (errno != 0 || opt.lwidth < 1 || opt.lwidth > 8)
+				die("sr: invalid option to -l: %s\n", value);
+			break;
+		case 2:
+			opt.lcolour = value;
+			break;
+		case 3:
+			opt.lalpha = parse_int(value);
+			if (errno != 0)
+				die("sr: invalid option to -l: %s\n", value);
+			break;
+		case 4:
+			opt.lmode = value;
+			if (strcmp(value, "classic") && strcmp(value, "edge"))
+				die("sr: invalid option to -l: %s\n", value);
+			break;
+		}
+	}
 }
 
-static bool optionsParseIsString(const char *const str)
+void
+options(int argc, char **argv, options_t *opt)
 {
-    return (str && (str[0] != '\0'));
-}
+	for (argv = &argv[1]; *argv != NULL; argv = &argv[1]) {
+		if ((*argv)[0] != '-')
+			die("sr: invalid argument: %s\n", *argv);
 
-static void optionsParseLine(char *optarg)
-{
-    enum {
-        Style = 0,
-        Width,
-        Color,
-        Opacity,
-        Mode
-    };
-
-    char *const token[] = {
-        [Style] = "style",
-        [Width] = "width",
-        [Color] = "color",
-        [Opacity] = "opacity",
-        [Mode] = "mode",
-        NULL
-    };
-
-    char *subopts = optarg;
-    char *value = NULL;
-
-    while (*subopts != '\0') {
-        switch (getsubopt(&subopts, token, &value)) {
-        case Style:
-            if (!optionsParseIsString(value)) {
-                errx(EXIT_FAILURE, "Missing value for suboption '%s'",
-                    token[Style]);
-            }
-
-            if (!strncmp(value, "dash", 4))
-                opt.lineStyle = LineOnOffDash;
-            else if (!strncmp(value, "solid", 5))
-                opt.lineStyle = LineSolid;
-            else {
-                errx(EXIT_FAILURE, "Unknown value for suboption '%s': %s",
-                    token[Style], value);
-            }
-            break;
-        case Width:
-            if (!optionsParseIsString(value)) {
-                errx(EXIT_FAILURE, "Missing value for suboption '%s'",
-                    token[Width]);
-            }
-
-            opt.lineWidth = optionsParseRequiredNumber(value);
-
-            if (opt.lineWidth <= 0 || opt.lineWidth > 8) {
-                errx(EXIT_FAILURE, "Value of the range (1..8) for "
-                    "suboption '%s': %d", token[Width], opt.lineWidth);
-            }
-            break;
-        case Color:
-            if (!optionsParseIsString(value)) {
-                errx(EXIT_FAILURE, "Missing value for suboption '%s'",
-                    token[Color]);
-            }
-
-            opt.lineColor = estrdup(value);
-            break;
-        case Mode:
-            if (!optionsParseIsString(value)) {
-                errx(EXIT_FAILURE, "Missing value for suboption '%s'",
-                    token[Mode]);
-            }
-
-            bool isValidMode = !strncmp(value, LINE_MODE_S_CLASSIC, LINE_MODE_L_CLASSIC);
-
-            isValidMode = isValidMode || !strncmp(value, LINE_MODE_S_EDGE, LINE_MODE_L_EDGE);
-
-            if (!isValidMode) {
-                errx(EXIT_FAILURE, "Unknown value for suboption '%s': %s",
-                    token[Mode], value);
-            }
-
-            opt.lineMode = estrdup(value);
-            break;
-        case Opacity:
-            if (!optionsParseIsString(value)) {
-                errx(EXIT_FAILURE, "Missing value for suboption '%s'",
-                    token[Opacity]);
-            }
-            opt.lineOpacity = optionsParseRequiredNumber(value);
-            break;
-        default:
-            errx(EXIT_FAILURE, "No match found for token: '%s'", value);
-            break;
-        }
-    } /* while */
-}
-
-void optionsParse(int argc, char *argv[])
-{
-    static char stropts[] = "a:ofipbcd:e:hmq:s::t:uvzn:l:D:k::C:S:F:";
-
-    static struct option lopts[] = {
-        /* actions */
-        { "focused", no_argument, 0, 'u' },
-        { "border", no_argument, 0, 'b' },
-        { "pointer", no_argument, 0, 'p' },
-        { "ignorekeyboard", no_argument, 0, 'i' },
-        { "freeze", no_argument, 0, 'f' },
-        /* toggles */
-        { "select", optional_argument, 0, 's' },
-        { "autoselect", required_argument, 0, 'a' },
-        { "display", required_argument, 0, 'D' },
-        { "line", required_argument, 0, 'l' },
-        { 0, 0, 0, 0 }
-    };
-    int optch = 0, cmdx = 0;
-
-    /* Now to pass some optionarinos */
-    while ((optch = getopt_long(argc, argv, stropts, lopts, &cmdx)) != EOF) {
-        switch (optch) {
-        case 0:
-            break;
-        case 'b':
-            opt.border = 1;
-            break;
-        case 's':
-			opt.selection.mode = SELECTION_MODE_CAPTURE;
-            break;
-        case 'u':
-            opt.focused = 1;
-            break;
-        case 'p':
-            opt.pointer = 1;
-            break;
-        case 'i':
-            opt.ignoreKeyboard = 1;
-            break;
-        case 'f':
-            opt.freeze = 1;
-            break;
-        case 'a': ;
-		    char *token;
-		    const char tokenDelimiter[2] = ",";
-		    int dimensions[4];
-		    int i = 0;
-		
-		    if (strchr(optarg, ',')) { /* geometry dimensions must be in format x,y,w,h   */
-		        dimensions[i++] = optionsParseRequiredNumber(strtok(optarg, tokenDelimiter));
-		        while ((token = strtok(NULL, tokenDelimiter)))
-		            dimensions[i++] = optionsParseRequiredNumber(token);
-		        opt.autoselect = 1;
-		        opt.autoselectX = dimensions[0];
-		        opt.autoselectY = dimensions[1];
-		        opt.autoselectW = dimensions[2];
-		        opt.autoselectH = dimensions[3];
-		
-		        if (i != 4)
-		            errx(EXIT_FAILURE, "option 'autoselect' require 4 arguments");
-		    } else
-		        errx(EXIT_FAILURE, "invalid format for option -- 'autoselect'");
-		            break;
-        case 'D':
-		    opt.display = strndup(optarg, MAX_DISPLAY_NAME);
-		    if (!opt.display)
-		        err(EXIT_FAILURE, "Unable to allocate display");
-		            break;
-        case 'l':
-            optionsParseLine(optarg);
-            break;
-        default:
-            break;
-        }
-    }
-
-    /* So that we can safely be called again */
-    optind = 1;
+		switch ((*argv)[1]) {
+		case 'a':
+			opt.asel = true;
+			parse_asel(opt, *(argv += sizeof(char *)));
+			break;
+		case 'b':
+			opt.border = true;
+			break;
+		case 'd':
+			opt.dpy = *(argv += sizeof(char *));
+			break;
+		case 'f':
+			opt.freeze = true;
+			break;
+		case 'l':
+			opt.line = true;
+			parse_line(opt, *(argv += sizeof(char *)));
+			break;
+		case 'p':
+			opt.ptr = true;
+			break;
+		case 's':
+			opt.select = true;
+			break;
+		case 'u':
+			opt.focus = true;
+			break;
+		}
+	}
 }
--- src/options.h
+++ src/options.h
@@ -1,66 +1,23 @@
-/* options.h
+/* sr - screenshot utility
+ * Copyright (C) 2022 ArcNyxx
+ * see LICENCE file for licensing information */
 
-Copyright 1999-2000 Tom Gilbert <tom@linuxbrit.co.uk,
-                                  gilbertt@linuxbrit.co.uk,
-                                  scrot_sucks@linuxbrit.co.uk>
-Copyright 2009      James Cameron <quozl@us.netrek.org>
-Copyright 2010      Ibragimov Rinat <ibragimovrinat@mail.ru>
-Copyright 2017      Stoney Sauce <stoneysauce@gmail.com>
-Copyright 2019-2022 Daniel T. Borelli <danieltborelli@gmail.com>
-Copyright 2020      Sean Brennan <zettix1@gmail.com>
-Copyright 2021      IFo Hancroft <contact@ifohancroft.com>
-Copyright 2021      Peter Wu <peterwu@hotmail.com>
-Copyright 2021      Wilson Smith <01wsmith+gh@gmail.com>
-Copyright 2021-2022 Guilherme Janczak <guilherme.janczak@yandex.com>
+#ifndef OPT_H
+#define OPT_H
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to
-deal in the Software without restriction, including without limitation the
-rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-sell copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+typedef struct options {
+	int lstyle, lwidth, lalpha;
+	bool asel, border, freeze, line, ptr, select, focus;
+	char *dpy, *lcolour, *lmode;
 
-The above copyright notice and this permission notice shall be included in
-all copies of the Software and its documentation and acknowledgment shall be
-given in the documentation and software packages that this Software was
-used.
+	union {
+		struct {
+			int x, y, w, h;
+		};
+		int arr[4];
+	};
+} options_t;
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+void options(int argc, char **argv, options_t *opts);
 
-*/
-
-#pragma once
-
-#include "scrot_selection.h"
-
-struct ScrotOptions {
-    int focused;
-    int border;
-    int pointer;
-    int ignoreKeyboard;
-    int freeze;
-    int lineStyle;
-    int lineWidth;
-    int lineOpacity;
-    char *lineColor;
-    char *lineMode;
-    char *display;
-    int autoselect;
-    int autoselectX;
-    int autoselectY;
-    int autoselectH;
-    int autoselectW;
-    SelectionMode selection;
-};
-
-extern struct ScrotOptions opt;
-
-void optionsParse(int, char **);
-void optionsParseAutoselect(char *);
-int optionsParseRequiredNumber(const char *);
-int optionsParseRequireRange(int, int, int);
+#endif /* OPT_H */
